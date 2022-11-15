804cfa830f6a9c5b03af25edb34086f0ec6c02ff
Heriberto Marcio <heribertomarcio@gmail.com>
2022-11-15 14:52:50 -0300

    Desafio Completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "modelo 1", IMEI: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "456789", modelo: "modelo 2", IMEI: "22222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+Console.WriteLine("\n");
+
+
+/*  // Testando acessibilidade de dados protegidos
+nokia.IMEI = "12345678";
+iphone.Memoria = 256;
+nokia.modelo = "ultimo modelo";
+*/
